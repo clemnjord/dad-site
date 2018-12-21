7751464caa83496370be52ef6e2bcffa3fed95e6
clemp <clempcontact@gmail.com>
2018-12-21 17:50:33 +0100

    CPU: Added the image viewer from https://github.com/fengyuanchen/viewerjs.      Added a page with the viewer simulating a "Autres Oeuvres" page.
    Signed-off-by: clemnjord <clempcontact@gmail.com>

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,13 @@ from app import app
 @app.route('/index')
 def index():
     return render_template('index.html', title="Alain Puech")
+
+@app.route('/images')
+def images():
+    #return 'images'
+    return render_template('oeuvres.html')
+
+@app.route('/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return render_template('oeuvres.html', image=post_id)
